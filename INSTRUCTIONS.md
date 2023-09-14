c6eb925329e3f7be7afc23182d02f70d30f30749
Robin Raabe <robinnraabe@gmail.com>
2023-09-13 22:27:08 -0500

    addEmployee now updates monthly cost and adds employees to DOM

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -23,7 +23,7 @@ No files have been provided (just instructions.md and a readme.md). Instead of f
 ![Wireframe](salary-calc-wireframe.png)
 *- Create HTML, JS, CSS files
 *- Creat form input field for first name, last name, ID#, job title, annual salary
-- Create submit button that collects/stores form info, calculates monthly costs, append info to DOM, clear fields
+*- Create submit button that collects/stores form info, calculates monthly costs, append info to DOM, clear fields
 - Boolean to check if monthly cost exceeds $20,000, changes bg color of total monthly cost
 - Create delete button to remove employee from DOM
 - Subtract deleted employee's monthly cost from total
--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 let employeeArray = [];
 let employeeObject = {};
+let monthlyCost = 0;
+let employeeDiv = document.querySelector("#employees");
 
 function addEmployee(event) {
     // Stores employee info
@@ -9,6 +11,8 @@ function addEmployee(event) {
     employeeObject.jobTitle = document.querySelector("#job-title-input").value;
     employeeObject.annualSalary = document.querySelector("#annual-salary-input").value;
     employeeArray.push(employeeObject);
+    // Updates total monthly cost
+    monthlyCost += employeeObject.annualSalary / 12;
     employeeObject = {};
     // Clears input fields
     document.querySelector("#first-name-input").value = "";
@@ -16,4 +20,15 @@ function addEmployee(event) {
     document.querySelector("#ID-number-input").value = "";
     document.querySelector("#job-title-input").value = "";
     document.querySelector("#annual-salary-input").value = "";
+    // Appends employees to DOM
+    for (let employee of employeeArray) {
+        employeeDiv.innerHTML += `
+        <tr>
+            <td> ${employee.firstName} </td>
+            <td> ${employee.lastName} </td>
+            <td> ${employee.idNumber} </td>
+            <td> ${employee.jobTitle} </td>
+            <td> ${employee.annualSalary} </td>
+        <tr>`
+    }
 }
